9b21d406a244c1133ceded12259e3dca136cdac5
the-ppz <frank1995alvcr@gmail.com>
2023-12-28 15:15:07 -0500

    Login y Logout en Reac, falta hacer UserView

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import ShowOrganizers from "./components/ShowOrganizers";
 import LoginAdm from "./components/LoginAdm";
+import LoginAs from "./components/LoginAs";
+import Asistente from "./components/asistente";
+import AsistenteRegistro from "./components/registroAsistente";
 
 function App() {
   return (
@@ -8,6 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<ShowOrganizers></ShowOrganizers>}></Route>
         <Route path="/loginadm/" element={<LoginAdm></LoginAdm>}></Route>
+        <Route path="/loginas/" element={<LoginAs></LoginAs>}></Route>
+        <Route path="/asistente/" element={<Asistente></Asistente>}></Route>
+        <Route path="/registroAsistente/" element={<AsistenteRegistro></AsistenteRegistro>}></Route>
       </Routes>
     </BrowserRouter>
   );
