2b3479f74872728c0ccb686b0c5a6adf61bc4a2b
styven26 <styvenpadilla@hotmail.com>
2023-12-29 17:40:06 -0500

    Login Admin Asistente Organizador Funcional

--- frontend/src/components/asistente.js
+++ frontend/src/components/asistente.js
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
 
 const Asistente = () => {
   const [asistenteData, setAsistenteData] = useState(null);
-  const navigate = useNavigate();
 
   useEffect(() => {
     fetchData();
