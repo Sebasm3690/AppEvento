46b9e9877185e74266370e02287b53d15851fc93
HeyMrDave <nuevobinalv@gmail.com>
2024-01-11 20:19:02 -0500

    Completo ci asistente

--- frontend/src/components/registroAsistente.js
+++ frontend/src/components/registroAsistente.js
@@ -3,17 +3,52 @@ import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
+// Función para validar la cédula
+function validarCedula(cedula) {
+  // Verificar que la cédula tenga 10 dígitos
+  if (cedula.length !== 10) {
+    return false;
+  }
+
+  // Obtener los primeros 9 dígitos y el dígito verificador
+  const digitos = cedula.substring(0, 9).split('').map(Number);
+  const digitoVerificador = parseInt(cedula.charAt(9), 10);
+
+  // Aplicar el algoritmo Módulo 10
+  let suma = 0;
+  for (let i = 0; i < 9; i++) {
+    let multiplicador = (i % 2 === 0) ? 2 : 1;
+    let resultado = digitos[i] * multiplicador;
+    suma += (resultado > 9) ? resultado - 9 : resultado;
+  }
+
+  let modulo = suma % 10;
+  let resultadoEsperado = (modulo === 0) ? 0 : 10 - modulo;
+
+  return resultadoEsperado === digitoVerificador;
+}
+
 const Registro = () => {
   const { register, handleSubmit, setError, formState: { errors } } = useForm();
   const navigate = useNavigate();
 
   const onSubmit = async (data) => {
+    // Validar la cédula
+    const isValidCedula = validarCedula(data.ci);
+
+    if (!isValidCedula) {
+      setError('ci', {
+        type: 'manual',
+        message: 'Número de cédula inválido',
+      });
+      return;
+    }
+
     try {
       const response = await axios.post('http://127.0.0.1:8000/api/register', data);
       console.log(response.data);
       alert('Gracias por tu registro, confirma tu correo para continuar');
-      navigate("/loginAs/")
-      // Aquí puedes añadir una redirección o cualquier otra lógica necesaria después del registro exitoso
+      navigate("/loginAs/");
     } catch (error) {
       setError('email', {
         type: 'manual',
@@ -49,7 +84,10 @@ const Registro = () => {
           </div>
           <div className="mb-3">
             <label>CI:</label>
-            <input {...register('ci', { required: 'Campo requerido' })} className="form-control" />
+            <input {...register('ci', { 
+              required: 'Campo requerido',
+              validate: value => validarCedula(value) || 'Número de cédula inválido' 
+            })} className="form-control" />
             {errors.ci && <p className="text-danger">{errors.ci.message}</p>}
           </div>
           <button type="submit" className="btn btn-primary">Registrarse</button>
@@ -60,3 +98,4 @@ const Registro = () => {
 };
 
 export default Registro;
+
