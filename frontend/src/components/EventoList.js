e7f4661434c4af1a730977a686ebaf67977b9ed2
Sebastian <anonblade10@gmail.com>
2024-01-19 08:47:52 -0500

    Carrusel y modal al ver eventos para asistente

--- frontend/src/components/EventoList.js
+++ frontend/src/components/EventoList.js
@@ -2,9 +2,25 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  FormGroup,
+  ModalFooter,
+} from "reactstrap";
+import { show_alerta } from "../functions";
+import { set } from "react-hook-form";
 
 function EventosList() {
   const [eventos, setEventos] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [id_evento, setId_evento] = useState(0);
+  const [fecha, setFecha] = useState("");
+  const [hora, setHora] = useState(0);
+  const [ubicacion, setUbicacion] = useState("");
+  const [descripcion, setDescripcion] = useState("");
 
   useEffect(() => {
     // Función para obtener los eventos desde tu API
@@ -21,8 +37,17 @@ function EventosList() {
     fetchEventos();
   }, []);
 
+  function handleMostrarDatos(evento) {
+    setFecha(evento.fecha);
+    setHora(evento.hora);
+    setUbicacion(evento.ubicacion);
+    setDescripcion(evento.descripcion);
+    setId_evento(evento.id_evento);
+    setShowModal(true);
+  }
+
   return (
-    <div>
+    <>
       <h1 style={{ textAlign: "center" }}>Listado de Eventos</h1>
       <div
         style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}
@@ -46,18 +71,105 @@ function EventosList() {
                   <p className="card-text">
                     <small className="text-muted">{evento.fecha}</small>
                   </p>
-                  <Link
-                    to={`/meventos/eventonum/${evento.id_evento}`}
+                  <Button
                     className="btn btn-primary"
+                    onClick={(e) => handleMostrarDatos(evento)}
                   >
-                    Ver más detalles
-                  </Link>
+                    Ver evento
+                  </Button>
                 </div>
               </div>
             </div>
           ))}
       </div>
-    </div>
+      {/*Ventana modal*/}
+
+      <Modal isOpen={showModal}>
+        <ModalHeader>
+          <div>
+            <h3>Información del evento</h3>
+          </div>
+        </ModalHeader>
+
+        {/* Vista previa de la imagen }
+
+        {image && (
+          <img
+            src={image}
+            alt="Imagen de vista previa"
+            style={{ maxWidth: "100%", height: "auto" }}
+          />
+        )}
+
+        {----------------------------*/}
+
+        <ModalBody>
+          <FormGroup>
+            <label>Id:</label>
+            <input
+              className="form-control"
+              readOnly
+              name="id"
+              type="text"
+              value={id_evento}
+            />
+          </FormGroup>
+          <FormGroup>
+            <label>Fecha:</label>
+            <input
+              className="form-control"
+              readOnly
+              name="fecha"
+              type="date"
+              onChange={(e) => setFecha(e.target.value)}
+              value={fecha}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <label>Hora:</label>
+            <input
+              className="form-control"
+              readOnly
+              name="hora"
+              type="text"
+              onChange={(e) => setHora(e.target.value)}
+              value={hora}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <label>Ubicacion:</label>
+            <input
+              className="form-control"
+              readOnly
+              name="ubicacion"
+              type="text"
+              onChange={(e) => setUbicacion(e.target.value)}
+              value={ubicacion}
+            />
+          </FormGroup>
+
+          <FormGroup>
+            <label>Descripcion:</label>
+            <input
+              className="form-control"
+              readOnly
+              name="descripcioin"
+              type="text"
+              onChange={(e) => setDescripcion(e.target.value)}
+              value={descripcion}
+            />
+          </FormGroup>
+        </ModalBody>
+
+        <ModalFooter>
+          <Link to={`/verboletos/${id_evento}`} className="btn btn-primary">
+            Ver más detalles
+          </Link>
+        </ModalFooter>
+      </Modal>
+    </>
   );
 }
 
