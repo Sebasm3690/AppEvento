49df015f8c0c02c4c7b0e6916016b69d1eaf947f
HeyMrDave <nuevobinalv@gmail.com>
2024-01-20 22:39:39 -0500

    Correcion ultima confirmacion correo

--- frontend/src/components/confirmado_correo.js
+++ frontend/src/components/confirmado_correo.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const ConfirmationPage = () => {
 
@@ -7,7 +8,7 @@ const ConfirmationPage = () => {
     <div>
     <h1>Correo confirmado exitosamente</h1>
     <p>¡Tu correo ha sido confirmado correctamente!</p>
-    {/* Otro contenido adicional si es necesario */}
+    <p>Ir a <Link to="/loginAs/">iniciar sesión</Link></p>
   </div>
   );
 };
