a73afd7f1384d908add3645fc73eeb1956fcba11
Franklin Alvarez <frank1995alvcr@gmail.com>
2024-01-30 02:19:45 -0500

    Diseño Compra

--- frontend/src/components/Admin/ShowOrganizers.js
+++ frontend/src/components/Admin/ShowOrganizers.js
@@ -12,6 +12,7 @@ import {
 } from "reactstrap";
 import { show_alerta } from "../../functions";
 import '../styles/showor.css'
+//import Sidebar from './sidebar';
 //import "./indexAdmin.css";
 
 export default function ShowOrganizers({ adminObj }) {
@@ -260,90 +261,90 @@ const CrudOrganizers = ({ adminObj }) => {
     <>
       <Container>
         <div style={{ paddingBlockEnd: '50px'}}>
-          <button
-            style={{ backgroundColor: '#2980b9', borderColor: '#2980b9', padding: '10px 20px', borderRadius: '8px' }}
-            className="btn btn-primary"
-            onClick={() => setShowModalInsert(true)}
-          >
-            INSERTAR ORGANIZADOR
-          </button> 
-          <span style={{ margin: '0 40px' }}></span>
-          <Button style={{ backgroundColor: '#6aabb5', borderColor: '#6aabb5', color: '#fff', padding: '10px 20px', borderRadius: '8px' }} className="btn btn-primary" onClick={handleRecuperar}>
-            RECUPERAR ORGANIZADOR
-          </Button>
+            <button
+              style={{ backgroundColor: '#2980b9', borderColor: '#2980b9', padding: '10px 20px', borderRadius: '8px' }}
+              className="btn btn-primary"
+              onClick={() => setShowModalInsert(true)}
+            >
+              INSERTAR ORGANIZADOR
+            </button> 
+            <span style={{ margin: '0 40px' }}></span>
+            <Button style={{ backgroundColor: '#6aabb5', borderColor: '#6aabb5', color: '#fff', padding: '10px 20px', borderRadius: '8px' }} className="btn btn-primary" onClick={handleRecuperar}>
+              RECUPERAR ORGANIZADOR
+            </Button>
         </div>
 
-          <Table className="table-custom table-borderless table-responsive">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Cédula de Identidad</th>
-                <th>Correo</th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {organizers
-                .filter((organizer) => !organizer.eliminado)
-                .filter((organizer) => organizer.id_admin === adminObj.id_admin)
-                .map((organizer) => (
-                  <tr key={organizer.id_organizador}>
-                    <td data-title="ID Organizador">{organizer.id_organizador}</td>
-                    <td data-title="Nombre">{organizer.nombre}</td>
-                    <td data-title="Apellido">{organizer.apellido}</td>
-                    <td data-title="Cédula de Identidad">{organizer.ci}</td>
-                    <td data-title="Correo">{organizer.correo}</td>
-                    <td>
-                      <div style={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
-                      
-                        <Button
-                          style={{
-                            color: "#fff",
-                            padding: "7px 14px",
-                            borderRadius: "8px",
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            backgroundColor: '#ffc107' // Color para el botón warning
-                          }}
-                          onClick={() =>
-                            handleEditarUsuario(organizer.id_organizador)
-                          }
-                          className="btn btn-warning"
-                        >
-                          <img src={"https://cdn-icons-png.flaticon.com/512/8188/8188360.png"} alt="Editar" width={'25px'} />
-                          <span style={{ margin: '0 3px'}}></span>
-                        </Button>
-                        <Button
-                          style={{
-                            color: "#fff",
-                            padding: "7px 14px",
-                            borderRadius: "8px",
-                            display: 'flex',
-                            alignItems: 'center',
-                            justifyContent: 'center',
-                            backgroundColor: '#dc3545' 
-                          }}
-                          className="btn btn-danger"
-                          onClick={() =>
-                            handleEliminarUsuario(organizer.id_organizador)
-                          }
-                        >
-                          <img
-                            src="https://cdn-icons-png.flaticon.com/512/2340/2340337.png"
-                            alt="Editar"
-                            width={"25px"}
-                          />
-                          <span style={{ margin: "0 3px" }}></span>
-                        </Button>
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </Table>
+            <Table className="table-custom table-borderless table-responsive">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nombre</th>
+                  <th>Apellido</th>
+                  <th>Cédula de Identidad</th>
+                  <th>Correo</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {organizers
+                  .filter((organizer) => !organizer.eliminado)
+                  .filter((organizer) => organizer.id_admin === adminObj.id_admin)
+                  .map((organizer) => (
+                    <tr key={organizer.id_organizador}>
+                      <td data-title="ID Organizador">{organizer.id_organizador}</td>
+                      <td data-title="Nombre">{organizer.nombre}</td>
+                      <td data-title="Apellido">{organizer.apellido}</td>
+                      <td data-title="Cédula de Identidad">{organizer.ci}</td>
+                      <td data-title="Correo">{organizer.correo}</td>
+                      <td>
+                        <div style={{ display: 'flex', alignItems: 'center', gap: '6px' }}>
+                        
+                          <Button
+                            style={{
+                              color: "#fff",
+                              padding: "7px 14px",
+                              borderRadius: "8px",
+                              display: 'flex',
+                              alignItems: 'center',
+                              justifyContent: 'center',
+                              backgroundColor: '#ffc107' // Color para el botón warning
+                            }}
+                            onClick={() =>
+                              handleEditarUsuario(organizer.id_organizador)
+                            }
+                            className="btn btn-warning"
+                          >
+                            <img src={"https://cdn-icons-png.flaticon.com/512/8188/8188360.png"} alt="Editar" width={'25px'} />
+                            <span style={{ margin: '0 3px'}}></span>
+                          </Button>
+                          <Button
+                            style={{
+                              color: "#fff",
+                              padding: "7px 14px",
+                              borderRadius: "8px",
+                              display: 'flex',
+                              alignItems: 'center',
+                              justifyContent: 'center',
+                              backgroundColor: '#dc3545' 
+                            }}
+                            className="btn btn-danger"
+                            onClick={() =>
+                              handleEliminarUsuario(organizer.id_organizador)
+                            }
+                          >
+                            <img
+                              src="https://cdn-icons-png.flaticon.com/512/2340/2340337.png"
+                              alt="Editar"
+                              width={"25px"}
+                            />
+                            <span style={{ margin: "0 3px" }}></span>
+                          </Button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+              </tbody>
+            </Table>
       </Container>
 
       <Modal isOpen={showModal}>
